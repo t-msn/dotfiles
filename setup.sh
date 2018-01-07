9ae86b6b9cefe1ffbd72f288f813567a225b0e6c
tm <misono.tm@gmail.com>
2018-01-07 19:48:48 +0900

    update

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ln -s "$PWD/.bashrc" ~/.bashrc
+ln -s "$PWD/.inputrc" ~/.inputrc
+ln -s "$PWD/.tmux.conf" ~/.tmux.conf
+
+# vim
+mkdir -p ~/.config/vim
+ln -s "$PWD/.vimrc" ~/.vimrc
+ln -s "$PWD/dein.toml" ~/.config/vim
+ln -s "$PWD/dein_lazy.toml" ~/.config/vim
+ln -s "$PWD/dein_misc.toml" ~/.config/vim
+
+# neovim
+mkdir -p ~/.config/nvim
+ln -s "$PWD/.init.vim" ~/.config/nvim
+ln -s "$PWD/dein_nvim.toml" ~/.config/nvim/
+ln -s "$PWD/dein_nvim_lazy.toml" ~/.config/nvim
