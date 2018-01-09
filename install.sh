3dd5fec3b5483d3a486ead7e0ad273ff716248f0
Tomohiro Misono <misono.tomohiro@jp.fujitsu.com>
2018-01-09 17:31:23 +0900

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 if [ -e /etc/fedora-release ]; then
-	sudo dnf install -y git vim-enhanced neovim tmux bash-completion
+	sudo dnf -y install git vim-enhanced neovim tmux bash-completion
+	sudo pip3 install neovim
+
+	# for deoplete
+	sudo pip3 install jedi
+	sudo dnf -y install clang
+
 	sudo dnf copr enable carlwgeorge/ripgrep
-	sudo dnf install -y ripgrep
-	sudo dnf install -y https://github.com/jhawthorn/fzy/releases/download/0.9/fzy-0.9-1.x86_64.rpm
+	sudo dnf -y install ripgrep
+	sudo dnf -y install https://github.com/jhawthorn/fzy/releases/download/0.9/fzy-0.9-1.x86_64.rpm
 
 	mkdir src
 	git clone https://github.com/b4b4r07/enhancd.git src/enhancd
